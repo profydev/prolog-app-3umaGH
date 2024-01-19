266960a4dbd79348082801ad638acd40fce69421
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-01-19 15:47:28 +0200

    feat: added mapper for API status responses

--- api/projects.ts
+++ api/projects.ts
@@ -1,9 +1,35 @@
 import { axios } from "./axios";
-import type { Project } from "./projects.types";
+import { Project, ProjectStatus } from "./projects.types";
 
 const ENDPOINT = "/project";
 
 export async function getProjects() {
   const { data } = await axios.get<Project[]>(ENDPOINT);
-  return data;
+
+  const mappedData = data.map((project) => ({
+    ...project,
+    status: mapProjectStatus(
+      project.status as keyof typeof ApiResponseStatuses,
+    ),
+  }));
+
+  return mappedData;
 }
+
+const ApiStatusToEnum = {
+  info: ProjectStatus.stable,
+  warning: ProjectStatus.warning,
+  error: ProjectStatus.critical,
+};
+
+const ApiResponseStatuses = {
+  info: "info",
+  warning: "warning",
+  error: "error",
+};
+
+const mapProjectStatus = (
+  status: keyof typeof ApiResponseStatuses,
+): ProjectStatus => {
+  return ApiStatusToEnum[status] || ProjectStatus.critical;
+};
